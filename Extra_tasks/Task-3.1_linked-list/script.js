ce50ac38b8ce0ef3f0135ae27b943522121d771a
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-03-24 15:38:04 +0300

    add method addNode

--- Extra_tasks/Task-3.1_linked-list/script.js
+++ Extra_tasks/Task-3.1_linked-list/script.js
@@ -1,3 +1,48 @@
-console.log(
-  'ff'
-);
+class Node {
+  constructor(value) {
+    this.number = value;
+    this.next = null;
+  }
+}
+
+class List {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  addNode(value, i) {
+    if (this.length === 0) {
+      this.head = new Node(value);
+    } else {
+      if (i < 0 || i > this.length) {
+        return false;
+      }
+      let node = new Node(value);
+      let cur;
+
+      if (i === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else if (i > 0) {
+        cur = this.head;
+        let prev = null;
+        let index = 0;
+        while (index < i) {
+          prev = cur;
+          cur = cur.next;
+          index++;
+        }
+        prev.next = node;
+        node.next = cur;
+      } else {
+        cur = this.head;
+        while (cur.next) {
+          cur = cur.next;
+        }
+        cur.next = new Node(value);
+      }
+      this.length++;
+      return true;
+    }
+  }
+}
