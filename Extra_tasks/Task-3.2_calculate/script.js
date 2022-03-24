a7505bae5ed6411279a8e04b1c5cbd249f3f11a2
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-03-24 17:51:22 +0300

    change functions to arrow

--- Extra_tasks/Task-3.2_calculate/script.js
+++ Extra_tasks/Task-3.2_calculate/script.js
@@ -1,25 +1,22 @@
-function add(a, b) {
-  return a + b;
-}
+const add = (a, b) => a + b;
 
-function mul(a, b) {
-  return a * b;
-}
+const mul = (a, b) => a * b;
 
-function sub(a, b) {
-  return a - b;
-}
+const sub = (b) => (a) => a - b;
+const sub1 = sub(1);
 
-function div(a, b) {
-  return a / b;
-}
+const div = (b) => (a) => a / b;
 
-let a = add(1, 2);
+const pipe =
+  (...fns) =>
+  (val) =>
+    fns.reduce((prev, fn) => fn(prev), val);
 
-let b = mul(a, 10);
-
-let sub1 = sub(1);
-
-// let c = sub1(b);
-
+let a = pipe((x) => add(1, x))(2);
+let b = pipe((x) => mul(a, x))(10);
+let c = pipe(sub1)(b);
+// let d = pipe(mul,sub(a,1))(c);
+console.log(a);
 console.log(b);
+console.log(c);
+// console.log(d);
