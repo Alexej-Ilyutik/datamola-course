3d2d641ddd1a095357abd66f765ced88d1290f8a
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-03-24 17:01:45 +0300

    add functions add, mul, sub, div

---
+++ Extra_tasks/Task-3.2_calculate/script.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+  return a + b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+let a = add(1, 2);
+
+let b = mul(a, 10);
+
+let sub1 = sub(1);
+
+// let c = sub1(b);
+
+console.log(b);
