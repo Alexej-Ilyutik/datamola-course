b108173c1b633ad87b859a9a638288c8532c9dd8
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-03-27 22:05:12 +0300

    add new solution

--- Extra_tasks/Task-3.2_calculate/script.js
+++ Extra_tasks/Task-3.2_calculate/script.js
@@ -1,22 +1,27 @@
-const add = (a, b) => a + b;
+const add = (b) => (a) => a + b;
 
-const mul = (a, b) => a * b;
+const mul = (b) => (a) => a * b;
 
 const sub = (b) => (a) => a - b;
-const sub1 = sub(1);
 
 const div = (b) => (a) => a / b;
 
-const pipe =
-  (...fns) =>
-  (val) =>
-    fns.reduce((prev, fn) => fn(prev), val);
+const reduced = (f, g) => (arg) => g(f(arg));
+const pipe = (...fns) => fns.reduce(reduced);
 
-let a = pipe((x) => add(1, x))(2);
-let b = pipe((x) => mul(a, x))(10);
+let a = add(1)(2);
+let b = mul(a)(10);
+const sub1 = sub(1);
 let c = pipe(sub1)(b);
-// let d = pipe(mul,sub(a,1))(c);
+let d = pipe(mul(sub(1)(a)))(c);
 console.log(a);
 console.log(b);
 console.log(c);
-// console.log(d);
+console.log(d);
+
+let doSmth = pipe(add(d), sub(c), mul(b), div(a));
+let result = doSmth(0);
+console.log(result);
+
+let x = pipe(add(1), mul(2))(3);
+console.log(x);
